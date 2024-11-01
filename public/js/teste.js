15e885e81d5f4d48b5f5be5550c8c002da667da8
Luccas Gomes Zibordi <luccaszibordi@gmail.com>
2024-11-01 11:02:40 -0300

    atualização da main page

--- public/js/teste.js
+++ public/js/teste.js
@@ -1,21 +1,9 @@
-let currentIndex = 0;
-const items = document.querySelectorAll(".carousel-item");
-
-function showSlide(index) {
-  items.forEach((item, i) => {
-    item.style.display = i === index ? "block" : "none";
-  });
-}
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel-slide");
 
 function moveSlide(direction) {
-  currentIndex += direction;
-  if (currentIndex < 0) {
-    currentIndex = items.length - 1;
-  } else if (currentIndex >= items.length) {
-    currentIndex = 0;
-  }
-  showSlide(currentIndex);
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+  document.querySelector(".carousel-track").style.transform = `translateX(-${
+    currentSlide * 100
+  }%)`;
 }
-
-// Initialize the carousel
-showSlide(currentIndex);
